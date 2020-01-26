fef62ceb8a07637f42a77e668a59a818896bf313
Jamie Won <jamie.won.2000@gmail.com>
2020-01-25 20:35:41 -0500

    Login firebase. last file

--- conuhacksV/src/environments/environment.ts
+++ conuhacksV/src/environments/environment.ts
@@ -1,10 +1,24 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+// import firebase from "firebase";
 export const environment = {
-  production: false
+  production: true,
+
+
+  // Your web app's Firebase configuration
+  firebase: {
+    apiKey: "AIzaSyByFNJ2leYqfWv12NISGIuY1O2EQiY1jzc",
+    authDomain: "jade-cgi.firebaseapp.com",
+    databaseURL: "https://jade-cgi.firebaseio.com",
+    projectId: "jade-cgi",
+    storageBucket: "jade-cgi.appspot.com",
+    messagingSenderId: "1070053375687",
+    appId: "1:1070053375687:web:e05fd3e0f2a26991b148e2",
+    measurementId: "G-Q4NP4MMY19"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
@@ -14,3 +28,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
